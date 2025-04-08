a62bdcb47d81a32477adcf76a4e09b5ed396964c
trongtinIUH <trantin1973@gmail.com>
2025-04-08 23:18:21 +0700

    thêm cấu hình configs cho các services và build thành công lên localhost 8761

--- product-service/src/main/java/fit/iuh/productservice/controllers/ProductController.java
+++ product-service/src/main/java/fit/iuh/productservice/controllers/ProductController.java
@@ -1,7 +1,7 @@
 package fit.iuh.productservice.controllers;
 
-import fit.iuh.productservice.entity.Product;
-import fit.iuh.productservice.repository.ProductRepository;
+
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
@@ -10,9 +10,6 @@ import java.util.List;
 @RestController
 @RequestMapping("/products")
 public class ProductController {
-
-    @Autowired
-    private ProductRepository repository;
 
    @GetMapping
     //list string
--- product-service/src/main/java/fit/iuh/productservice/repository/ProductRepository.java
+++
@@ -1,6 +0,0 @@
-package fit.iuh.productservice.repository;
-
-import fit.iuh.productservice.entity.Product;
-import org.springframework.data.jpa.repository.JpaRepository;
-
-public interface ProductRepository extends JpaRepository<Product, Long> {}
